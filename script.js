153a69d888066c4c63ed59d46114dc22932f6829
anuragshukla28 <anuragshukla0728@gmail.com>
2025-06-16 19:48:50 +0530

    Adding functionality to Task button

---
+++ script.js
@@ -0,0 +1,27 @@
+//  Grab the input where we want to do something
+const todoInput = document.getElementById("todo-input");
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks=[];
+
+addTaskButton.addEventListener('click',() => {
+    // trim cut extra spaces at the end
+   const taskText= todoInput.value.trim();
+   if(taskText === ""){
+    return;
+   }
+   const newTask={
+    id:Date.now(),
+    text:taskText,
+    completed:false
+
+   }
+   tasks.push(newTask)
+   todoInput.value="";// clear input
+
+   console.log(tasks);
+   
+
+
+});
